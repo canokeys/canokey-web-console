7af8a4b0359acd71f92ce338a29aca208991ee68
Jiajie Chen <c@jia.je>
2020-05-17 15:00:18 +0800

    Add nested applet list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import logo from './logo.png';
-import { Toolbar, AppBar, IconButton, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText, Divider, Button,  } from '@material-ui/core';
-import { Route, Switch } from 'react-router-dom';
+import { Toolbar, AppBar, IconButton, Typography, Drawer, List, ListItem, ListItemIcon, ListItemText, Divider, Button, Collapse, } from '@material-ui/core';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import MenuIcon from '@material-ui/icons/Menu';
 import HomeIcon from '@material-ui/icons/Home';
+import AppsIcon from '@material-ui/icons/Apps';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import { connect, disconnect } from './actions';
 import Overview from './Overview';
 
@@ -22,13 +26,28 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: "10px",
     width: "70%",
   },
+  nested: {
+    paddingLeft: theme.spacing(5),
+  },
 }));
 
 export default function App() {
   const classes = useStyles();
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [appletOpen, setAppletOpen] = useState(true);
   const dispatch = useDispatch();
+  const history = useHistory();
   const device = useSelector(state => state.device);
+
+  const showOverview = useCallback(e => {
+    setDrawerOpen(false);
+    history.push('/');
+  }, [history]);
+
+  const showAdmin = useCallback(e => {
+    setDrawerOpen(false);
+    history.push('/admin');
+  }, [history]);
 
   return (
     <div className={classes.root}>
@@ -41,7 +60,7 @@ export default function App() {
             CanoKey Web Console
           </Typography>
           {
-            device  ?
+            device ?
               <Button color="inherit" aria-label="menu" onClick={() => dispatch(disconnect())}>
                 Disconnect
           </Button> :
@@ -57,35 +76,44 @@ export default function App() {
         <img src={logo} alt="logo" className={classes.icon} />
         <List>
           <Divider />
-          <ListItem button>
+          <ListItem button onClick={showOverview}>
             <ListItemIcon><HomeIcon /></ListItemIcon>
             <ListItemText>Overview</ListItemText>
           </ListItem>
-          <ListItem button>
-            <ListItemIcon><MenuIcon /></ListItemIcon>
-            <ListItemText>Admin</ListItemText>
+          <ListItem button onClick={() => setAppletOpen(!appletOpen)}>
+            <ListItemIcon><AppsIcon /></ListItemIcon>
+            <ListItemText>Applets</ListItemText>
+            {appletOpen ? <ExpandLessIcon /> : <ExpandMoreIcon /> }
           </ListItem>
-          <ListItem button>
-            <ListItemIcon><MenuIcon /></ListItemIcon>
-            <ListItemText>CTAP</ListItemText>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon><MenuIcon /></ListItemIcon>
-            <ListItemText>OATH</ListItemText>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon><MenuIcon /></ListItemIcon>
-            <ListItemText>OpenPGP</ListItemText>
-          </ListItem>
-          <ListItem button>
-            <ListItemIcon><MenuIcon /></ListItemIcon>
-            <ListItemText>PIV</ListItemText>
-          </ListItem>
+          <Collapse in={appletOpen}>
+            <List>
+              <ListItem button onClick={showAdmin} className={classes.nested}>
+                <ListItemIcon><ArrowForwardIcon /></ListItemIcon>
+                <ListItemText>Admin</ListItemText>
+              </ListItem>
+              <ListItem button className={classes.nested}>
+                <ListItemIcon><ArrowForwardIcon /></ListItemIcon>
+                <ListItemText>CTAP</ListItemText>
+              </ListItem>
+              <ListItem button className={classes.nested}>
+                <ListItemIcon><ArrowForwardIcon /></ListItemIcon>
+                <ListItemText>OATH</ListItemText>
+              </ListItem>
+              <ListItem button className={classes.nested}>
+                <ListItemIcon><ArrowForwardIcon /></ListItemIcon>
+                <ListItemText>OpenPGP</ListItemText>
+              </ListItem>
+              <ListItem button className={classes.nested}>
+                <ListItemIcon><ArrowForwardIcon /></ListItemIcon>
+                <ListItemText>PIV</ListItemText>
+              </ListItem>
+            </List>
+          </Collapse>
         </List>
       </Drawer>
 
       <Switch>
-        <Route path="/" exact component={Overview}/>
+        <Route path="/" exact component={Overview} />
       </Switch>
     </div>
   );
