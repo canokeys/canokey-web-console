7d73e3552a9d1645104e7b3ce56467b0e30c94c5
dangfan <i@dang.fan>
2020-05-28 23:41:58 +0000

    Deploying to gh-pages from master @ b3919e0d87fa8afe5badccf1e72ab71e74323eb6 🚀

---
+++ precache-manifest.9bd7c39bc84d32e21722027ec0cf5c7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f83e987300676007533a8de09a7771",
+    "url": "/canokey-web-console/index.html"
+  },
+  {
+    "revision": "f50a40120c2a8374f60a",
+    "url": "/canokey-web-console/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82fa84737b91a1817c09",
+    "url": "/canokey-web-console/static/js/2.f768cf9f.chunk.js"
+  },
+  {
+    "revision": "fad87cf0013fd97dedb77bc8bbb13446",
+    "url": "/canokey-web-console/static/js/2.f768cf9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50a40120c2a8374f60a",
+    "url": "/canokey-web-console/static/js/main.384ab22c.chunk.js"
+  },
+  {
+    "revision": "4075f246e27caa2ee643",
+    "url": "/canokey-web-console/static/js/runtime-main.76c44f75.js"
+  }
+]);
